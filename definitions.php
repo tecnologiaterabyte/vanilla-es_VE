68c576b4ceec06c1297ed7932cdd116dfb6a4e79
Wilmer Arambula <tecnologiaterabyte@gmail.com>
2017-09-24 11:01:26 -0400

    Initial Commit

---
+++ definitions.php
@@ -0,0 +1,16 @@
+<?php
+
+ $LocaleInfo['vf_es_VE'] = array (
+  'Locale' => 'es_VE',
+  'Name' => 'Español (Venezuela) / Spanish (Venezuela)',
+  'EnName' => 'Spanish (Venezuela)',
+  'Description' => 'Official Spanish (Venezuela) language translations for Vanilla.',
+  'Version' => '2017.09.24-0.1',
+  'Author' => 'Ing. Wilmer Arambula',
+  'AuthorUrl' => 'https://github.com/tecnologiaterabyte/vanilla-es_VE',
+  'License' => 'CC BY-SA 4.0',
+  'PercentComplete' => 100,
+  'NumComplete' => 2216,
+  'DenComplete' => 2251,
+  'Icon' => 'es_VE.png',
+);
